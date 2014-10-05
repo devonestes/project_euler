a6c0e2a80e70d18786c0d1aff06fe78ca8c87e64
Devon Estes <devon.c.estes@gmail.com>
2014-10-05 11:35:45 -0700

    initial commit

---
+++ lib/3_euler.rb
@@ -0,0 +1,30 @@
+	# The prime factors of 13195 are 5, 7, 13 and 29.
+	# What is the largest prime factor of the number 600851475143 ?
+
+require 'prime'
+
+def euler_3(input_int)
+	# create a list of prime numbers from 1 to 100
+	input_array = [input_int]
+	primesArray = Prime.take_while {|p| p < Math.sqrt(input_int) }
+	results = reduce(input_array, primesArray)
+	return results
+end
+
+def reduce(results, primes)
+	# iterate through prime number list to see if input_int % prime == 0
+	results.each_index { |index|
+		primes.each { |prime|
+			if results[index] == prime
+			elsif results[index] % prime == 0
+			results[index] = results[index]/prime
+			results << prime
+			end
+		}
+	}
+end
+
+print euler_3(600851475143)
+puts ""
+
+
