0cb3355682197b89882e823679d173eac464f07b
Devon Estes <devon.c.estes@gmail.com>
2014-10-05 22:00:13 -0700

    add solution to problem 11

---
+++ lib/11_euler.rb
@@ -0,0 +1,79 @@
+# In the 20×20 grid below, four numbers along a diagonal line have been marked in red.
+
+# The product of these numbers is 26 × 63 × 78 × 14 = 1788696.
+
+# What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20×20 grid?
+
+require 'Matrix'
+
+def euler()
+
+arr1 =  [8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 07, 78, 52, 12, 50, 77, 91, 8]
+arr2 =  [49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 4, 56, 62, 0]
+arr3 =  [81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 3, 49, 13, 36, 65]
+arr4 =  [52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91]
+arr5 =  [22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80]
+arr6 =  [24, 47, 32, 60, 99, 3, 45, 2, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50]
+arr7 =  [32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70]
+arr8 =  [67, 26, 20, 68, 2, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21]
+arr9 =  [24, 55, 58, 05, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72]
+arr10 = [21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95]
+arr11 = [78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92]
+arr12 = [16, 39, 05, 42, 96, 35, 31, 47, 55, 58, 88, 24, 0, 17, 54, 24, 36, 29, 85, 57]
+arr13 = [86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58]
+arr14 = [19, 80, 81, 68, 5, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 4, 89, 55, 40]
+arr15 = [04, 52, 8, 83, 97, 35, 99, 16, 07, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66]
+arr16 = [88, 36, 68, 87, 57, 62, 20, 72, 03, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69]
+arr17 = [4, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36]
+arr18 = [20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 4, 36, 16]
+arr19 = [20, 73, 35, 29, 78, 31, 90, 1, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 5, 54]
+arr20 = [1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 1, 89, 19, 67, 48]
+
+matrix = Matrix[arr1, arr2, arr3, arr4, arr5, arr6, arr7, arr8, arr9, arr10, arr11, arr12, arr13, arr14, arr15, arr16, arr17, arr18, arr19, arr20]
+answer = 1
+total = 1
+
+# try horizontal
+matrix.each_with_index do |num, row, column|
+  if row <= 16
+    total = num * matrix[row+1, column] * matrix[row+2, column] * matrix[row+3, column]
+    if total > answer
+      answer = total
+    end
+  end
+end
+
+# try vertical
+matrix.each_with_index do |num, row, column|
+  if column <= 16
+    total = num * matrix[row, column+1] * matrix[row, column+2] * matrix[row, column+3]
+    if total > answer
+      answer = total
+    end
+  end
+end
+
+# try l-r diagonal
+matrix.each_with_index do |num, row, column|
+  if row <= 16 && column <= 16
+    total = num * matrix[row+1, column+1] * matrix[row+2, column+2] * matrix[row+3, column+3]
+    if total > answer
+      answer = total
+    end
+  end
+end
+
+# try r-l diagonal
+matrix.each_with_index do |num, row, column|
+  if row <= 16 && column >= 3
+    total = num * matrix[row+1, column-1] * matrix[row+2, column-2] * matrix[row+3, column-3]
+    if total > answer
+      answer = total
+    end
+  end
+end
+
+answer
+end
+
+p euler()
