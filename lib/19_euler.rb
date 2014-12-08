8644a4c4964038bd55eaa73a262c967bab6666fe
Devon Estes <devon.c.estes@gmail.com>
2014-12-08 14:03:31 -0800

    adding solution to number 19

--- lib/19_euler.rb
+++ lib/19_euler.rb
@@ -1,11 +1,19 @@
-# You are given the following information, but you may prefer to do some research for yourself.
+regular_year  = [31,28,31,30,31,30,31,31,30,31,30,31]
+leap_year     = [31,29,31,30,31,30,31,31,30,31,30,31]
 
-# 1 Jan 1900 was a Monday.
-# Thirty days has September,
-# April, June and November.
-# All the rest have thirty-one,
-# Saving February alone,
-# Which has twenty-eight, rain or shine.
-# And on leap years, twenty-nine.
-# A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
-# How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+monday_counter = 0
+start_day = 1 #January 1, 1901 was a Tuesday
+(1901..2000).each do |year|
+  if year % 4 == 0
+    days_in_months = leap_year
+  else
+    days_in_months = regular_year
+  end
+  days_in_months.each do |month|
+    monday_counter += 1 if start_day == 6
+    first_of_next_month = (start_day + (month % 7)) % 7
+    start_day = first_of_next_month
+  end
+end
+
+p monday_counter
