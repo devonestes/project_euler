d6196a0a83f6f0efcb79cbd187f540d7bf8a8146
Devon Estes <devon.c.estes@gmail.com>
2014-12-08 14:46:35 -0800

    adding solution for 21

--- lib/21_euler.rb
+++ lib/21_euler.rb
@@ -4,3 +4,31 @@
 # For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
 
 # Evaluate the sum of all the amicable numbers under 10000.
+
+def d(n)
+  divisors = []
+  (1...n).each do |number|
+    divisors << number if n % number == 0
+  end
+  divisors.reduce(:+)
+end
+
+def create_hash
+  d_hash = {}
+  (2..9999).each do |num|
+    d_hash[num] = d(num)
+  end
+  d_hash
+end
+
+def amicable
+  amicable = []
+  d_hash = create_hash
+  d_hash.each do |k,v|
+    amicable << k if d_hash[v] == k && d_hash[v] != v
+  end
+  p amicable
+  amicable.inject(:+)
+end
+
+p amicable
