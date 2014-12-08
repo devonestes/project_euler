768c4c11e1661be290d3e7fd0cccb3db6cf31c1a
Devon Estes <devon.c.estes@gmail.com>
2014-12-08 15:08:20 -0800

    adding solution to 22

--- lib/22_euler.rb
+++ lib/22_euler.rb
@@ -3,3 +3,32 @@
 # For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
 
 # What is the total of all the name scores in the file?
+require 'csv'
+
+def create_letter_hash
+  letter_hash = {}
+  ('A'..'Z').each_with_index do |letter, index|
+    letter_hash[letter] = index + 1
+  end
+  letter_hash
+end
+
+def calc_name_score(name)
+  total = 0
+  letter_hash = create_letter_hash
+  name.split("").each do |letter|
+    total += letter_hash[letter]
+  end
+  total
+end
+
+def calc_all_names
+  names = CSV.readlines('p022_names.txt').flatten.sort
+  total = 0
+  names.each_with_index do |name, index|
+    total += (calc_name_score(name) * (index + 1))
+  end
+  total
+end
+
+p calc_all_names
