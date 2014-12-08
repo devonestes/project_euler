35ea94193f6aeda9e0c95d223b78f87d0da75299
Devon Estes <devon.c.estes@gmail.com>
2014-12-08 14:14:37 -0800

    adding solution to 20

--- lib/20_euler.rb
+++ lib/20_euler.rb
@@ -1,6 +1,6 @@
-# n! means n × (n − 1) × ... × 3 × 2 × 1
+def factorial(int)
+  return 1 if int == 1
+  int * factorial(int-1)
+end
 
-# For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
-# and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
-
-# Find the sum of the digits in the number 100!
+factorial(100).to_s.split("").inject(0) { |memo, int| memo + int.to_i }
